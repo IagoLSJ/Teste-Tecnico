941645366d0cd000401ca1cc1b8cd63f80038351
IagoLSJ <joseiagolima@alu.ufc.br>
2024-11-15 18:48:32 -0300

    chore: inicio do projeto

---
+++ src/main/java/com/dev/elevar/ElevarApplication.java
@@ -0,0 +1,13 @@
+package com.dev.elevar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/elevar/ElevarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.elevar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
